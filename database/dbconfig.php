d114f3392177d78fdb403372a2bb7d83af3a75a8
Steven Lord <SELord@users.noreply.github.com>
2020-04-13 23:22:46 -0700

    revert to blank credentials

--- database/dbconfig.php
+++ database/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 	//elaine db - connect to server and select database
 	$dbhost = 'oniddb.cws.oregonstate.edu';
-	$dbname = 'ohsa-db';
-	$dbuser = 'ohsa-db';
-	$dbpass = 't4zhQD9eM7cffKGq';
+	$dbname = '';
+	$dbuser = '';
+	$dbpass = '';
 ?>
