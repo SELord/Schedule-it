ed1c8203b4faf6934c92c07ba388e15e165f1360
Steven Lord <SELord@users.noreply.github.com>
2020-04-13 23:26:56 -0700

    Update comment

--- database/dbconfig.php
+++ database/dbconfig.php
@@ -1,5 +1,5 @@
 <?php
-	//elaine db - connect to server and select database
+	//Using Ohsa's db for production - connect to server and select database
 	$dbhost = 'oniddb.cws.oregonstate.edu';
 	$dbname = '';
 	$dbuser = '';
