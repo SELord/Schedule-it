ba0c23e3f02071ebbc5fd996547168486c41cc35
ohsa <litarrow@gmail.com>
2020-04-15 16:17:22 -0700

    fix accidental replace

--- file_path.php
+++ file_path.php
@@ -1,5 +1,5 @@
 <?php
     //Variable to be used in other files for webpage routing
     $DEV_ONID = "ohsa";
-    $FILE_PATH = "" . $FILE_PATH . "";
+    $FILE_PATH = "http://web.engr.oregonstate.edu/~" . $DEV_ONID . "/Schedule-it/";
 ?>
