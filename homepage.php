f61026fc2920da9e591ec90b44cf7b0535c92eb1
Victoria Fisher <fisherv@oregonstate.edu>
2020-05-04 20:13:18 -0400

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -179,7 +179,8 @@
                 <li class="nav-item">
                   <a href="view_history.php" class="nav-link">Past Meetings</a>
                 </li>
-                　　　　　　　　　　　　　　　　　
+                <!-- Temporary spacing fix -->
+                　　　　　　　　　　　　　　　　　　　　
                 <li class="nav-item">
                   <a href="logout.php" class="nav-link">Logout</a>
                 </li>
