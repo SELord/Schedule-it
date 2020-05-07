a15013dda3bd1cb4faa2a562082de51e2c2bd29f
Steven <lords@oregonstate.edu>
2020-05-06 22:56:57 -0700

    comment out buggy script injection

--- file_path.php
+++ file_path.php
@@ -4,6 +4,7 @@
     $FILE_PATH = "http://web.engr.oregonstate.edu/~" . $DEV_ONID . "/Schedule-it/";
 
     //urlPrefix variable for use in assets/js/event.js and other javascript files where paths are used
+    // Commented out because this introduced a bug where pages would not load
     //echo "<script>\n";
     //echo "var urlPrefix = " . json_encode($FILE_PATH) . ";";
     //echo "</script>";
