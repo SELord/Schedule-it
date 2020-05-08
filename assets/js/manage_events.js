ea13354103d3a86a85241cf9c5e73cd038f7973f
Steven <lords@oregonstate.edu>
2020-05-08 00:32:34 -0700

    remove event, reservation, invite buttons

--- assets/js/manage_events.js
+++ assets/js/manage_events.js
@@ -29,50 +29,8 @@ function createdEventHist() {
 }
 
 
-function inviteHist() {
-	let mostRecent = pastInvites[pastInvites.length - 1]['start']; // fullcalendar.io can handle getting the date from a string with the date and time
-    let calendarE1 = document.getElementById('content');
-    let calendar = new FullCalendar.Calendar(calendarE1, {
-        plugins: [ 'list' ],
-
-        header: {
-		  left: '',
-          center: 'title',
-		  right: 'listDay, listWeek, listMonth, prev, next'
-        },
-		
-		// customize button names
-		views: {
-			listDay: { buttonText: 'Day'}, 
-			listWeek: { buttonText: 'Week'},
-			listMonth: { buttonText: 'Month'},
-		},
-		
-        defaultView: 'listWeek',
-        defaultDate: mostRecent,
-        navLinks: true,  //can click day/week names to navigate views
-        editable: false,
-        eventLimit: true, //allow "more" link when too many events
-        events: pastInvites,
-    });
-    calendar.render();
-}
-
-
-function showInviteHist(event){
-	event.stopPropagation();
-	document.getElementById("inviteHistButton").disabled = true;
-	document.getElementById("eventHistButton").disabled = false;
-	document.getElementById("viewTitle").innerHTML = 'All Events You Are Invited To';
-	document.getElementById("content").innerHTML = "";
-	inviteHist();
-	
-}
-
 function showEventHist(event){
 	event.stopPropagation();
-	document.getElementById("eventHistButton").disabled = true;
-	document.getElementById("inviteHistButton").disabled = false;
 	document.getElementById("viewTitle").innerHTML = 'All Events You Created';
 	document.getElementById("content").innerHTML = "";
 	createdEventHist();
