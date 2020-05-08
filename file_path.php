bcbf00046cbfd79578da0e3280e312fdaac4a0f9
Steven <lords@oregonstate.edu>
2020-05-07 22:19:38 -0700

    change dev onid

--- file_path.php
+++ file_path.php
@@ -1,6 +1,6 @@
 <?php
     //Variable to be used in other files for webpage routing
-    $DEV_ONID = "ohsa";
+    $DEV_ONID = "lords";
     $FILE_PATH = "http://web.engr.oregonstate.edu/~" . $DEV_ONID . "/Schedule-it/";
 
     //urlPrefix variable for use in assets/js/event.js and other javascript files where paths are used
