25ccdb239efb2316f4644175bb3e3b35cb258c27
ohsa <litarrow@gmail.com>
2020-05-18 00:54:07 -0700

    "Create Event" button for eventmanagement page

--- assets/js/manage_events_main.js
+++ assets/js/manage_events_main.js
@@ -25,6 +25,57 @@ function createdEventHist() {
         eventLimit: true, //allow "more" link when too many events
         events: pastEvents,
     });
+
+    // create event button
+	document.getElementById('createEventDiv').innerHTML = '<right><button type="button" class="btn btn-large" id="createEvent">Create Event</button><br />';
+	$('#createEvent').click(function(){
+        console.log("loc1");
+		$( "#dialog-form" ).dialog();
+	});
+
+	// add days to a date
+	// reference: https://stackoverflow.com/questions/563406/add-days-to-javascript-date
+	function addDays(date, days) {
+		var result = new Date(date);
+		result.setDate(result.getDate() + days);
+		// reference: https://stackoverflow.com/questions/23593052/format-javascript-date-as-yyyy-mm-dd
+		const offset = result.getTimezoneOffset()
+		result = new Date(result.getTime() + (offset*60*1000))
+		return result.toISOString().split('T')[0]
+	}
+	
+    //BUTTON TO CREATE NEW EVENT - SUBMIT BUTTON IN CREATE_EVENT.PHP
+    $('#signupbtn').on('click',function(e){
+        e.preventDefault();
+        var title = $('#title').val();
+        var description = $('#description').val();
+        //get correct date format
+        //var date = dateStr;
+        var dateStart = $('#dateStart').val();
+        var dateEnd = $('#dateEnd').val();
+        dateEnd = addDays(dateEnd, 1);      // +1 day for fullcalendar display
+        //var slots = $('#slots').val();
+        //var RSVPslotLim = $('#RSVPslotLim').val();
+        var creatorID = $('#creatorID').val();    
+        var location = $('#location').val();
+        //var RSVPLim = $('#RSVPLim').val();
+        $.ajax({
+            url:"../Schedule-it/database/event/insert.php",
+            type:"POST",
+            data: {title:title, description:description, dateStart:dateStart, dateEnd:dateEnd, creatorID:creatorID, location:location},
+            complete: function() {
+                $( "#dialog-form" ).dialog( "close" );
+            },
+            success: function(){
+                calendar.refetchEvents();
+                alert("Added Successfully");
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        })
+    });
+
     calendar.render();
 }
 
