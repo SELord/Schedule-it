4c092781fb30614f64b5122d4930f221fcdc9e73
Victoria Fisher <fisherv@oregonstate.edu>
2020-05-19 04:06:35 -0400

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -267,6 +267,19 @@
 
       <!-- Allow form submission with keyboard without duplicating the dialog button -->
       <input type="button" value="Submit" id="signupbtn" onclick="submitForm()">
+
+      <script>
+      function submitForm() {
+   // Get the first form with the name
+   // Usually the form name is not repeated
+   // but duplicate names are possible in HTML
+   // Therefore to work around the issue, enforce the correct index
+   var frm = document.getElementsByName('calcreate')[0];
+   frm.submit(); // Submit the form
+   frm.reset();  // Reset all form data
+   return false; // Prevent page refresh
+}
+</script>
     </fieldset>
   </form>
 </div>
