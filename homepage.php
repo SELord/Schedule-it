fae6f5d983df3d50ba66f292d788d8c91156adad
Victoria Fisher <fisherv@oregonstate.edu>
2020-05-19 05:10:26 -0400

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -99,6 +99,7 @@
 	<!-- javascript files -->
 	<script src="./assets/js/main.js"></script>
 	<script src="./assets/js/view_history.js"></script>
+	<script src="./assets/js/event.js"></script> 
 
 
 	<!-- fontawesome for icon usage eg. navbar hamburger icon -->
@@ -252,10 +253,7 @@ div#users-contain table td, div#users-contain table th { border: 1px solid #eee;
       <label for="dateEnd">End Date: </label>
           <input type="date" name="dateEnd" id="dateEnd" class="text ui-widget-content ui-corner-all" required>
           <!--THIS IS CREATOR_ID -- SHOULD GET FROM SESSION -->
-      <input type="hidden" name="creatorID" id="creatorID" value="<?php echo $user->id;?>" />   
-
-          <!--THIS IS CREATOR_ID -- SHOULD GET FROM SESSION -->
-      <input type="hidden" name="creatorID" id="creatorID" value="<?php echo $user->id;?>" />   
+      <input type="hidden" name="creatorID" id="creatorID" value="<?php echo $user->id;?>" />     
 
       <!-- Allow form submission with keyboard without duplicating the dialog button -->
       <input type="submit" id="signupbtn">
