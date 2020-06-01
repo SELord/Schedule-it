eafcd1eab15315303b7fc73720c1422fb4a366b3
SELord <14917495+SELord@users.noreply.github.com>
2020-06-01 12:51:34 -0700

    switch to production file path

--- file_path.php
+++ file_path.php
@@ -8,13 +8,13 @@
     //Variable to be used in other files for webpage routing
     
     // Variable used on the production server. Uncomment for production 
-    //$FILE_PATH = "https://eecs.oregonstate.edu/education/scheduleit/";
+    $FILE_PATH = "https://eecs.oregonstate.edu/education/scheduleit/";
     
-    // Variable used in development on OSU flip servers. 
-    // Replace $DEV_ONID with your onid. 
-    // Comment out code when moved to production server.
-    $DEV_ONID = "lords"; // for dev env
-    $FILE_PATH = "http://web.engr.oregonstate.edu/~" . $DEV_ONID . "/scheduleit/";  // for dev env
+    /* Variable used in development on OSU flip servers. 
+    *  Replace $DEV_ONID with your onid. 
+    *  Comment out code when moved to production server.*/
+    //$DEV_ONID = "ohsa"; // for dev env
+    //$FILE_PATH = "http://web.engr.oregonstate.edu/~" . $DEV_ONID . "/scheduleit/";  // for dev env
 
     //urlPrefix variable for use in assets/js/event.js and other javascript files where paths are used
     // Commented out because this introduced a bug where pages would not load
